a2360c13bb1a917a18d86bfacc19a51f2bb68352
Suraj Borse <surajborse126097@gmail.com>
2026-09-16 12:25:48 +0530

    remove unnecessary code

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,5 @@
-import { Link, Route, Routes } from 'react-router-dom'
-import Labels from './Labels'
 import Scanner from './Scanner'
-import './nav.css'
 
 export default function App() {
-  return (
-    <>
-      <nav className="app-nav">
-        <Link to="/scan">Scanner</Link>
-        <Link to="/labels">Labels</Link>
-      </nav>
-      <Routes>
-        <Route path="/" element={<Scanner />} />
-        <Route path="/scan" element={<Scanner />} />
-        <Route path="/labels" element={<Labels />} />
-      </Routes>
-    </>
-  )
+  return <Scanner />
 }
